c75cd9ac80f275c7801d9bb44e7a49f41c72cfab
RealJammy <57639251+RealJammy@users.noreply.github.com>
2020-08-11 23:51:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-
+import random
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -14,11 +14,17 @@ async def on_member_join(member):
 
 @client.event
 async def on_member_remove(member):
-  print(f'{member} has left the server. Goodbye!')
+  print(f'{member} has left the server.Goodbye!')
+
+@client.event
+async def on_command_error(ctx, error):
+  if isinstance(error, commands.MissingRequiredArgument):
+    await ctx.send('Please give all the arguments.')
+
 
 
 @client.command()
-async def anime(ctx):
+async def noanime(ctx):
   await ctx.send('No anime!')
 
 @client.command()
@@ -28,29 +34,31 @@ async def ping(ctx):
 @client.command()
 async def lyne(ctx):
   await ctx.send('http://www.risk-uk.com/wp-content/uploads/2015/04/JamesLyneSANSCyberAcademy1.png')
-
-@client.command()
-async def github(ctx):
-  await ctx.send('https://github.com/RealJammy/The-Jambot/blob/master/README.md')
-
-@client.command()
-async def noleek(ctx):
-  await ctx.send('https://game.joincyberdiscovery.com/assets/videos/cheating_message.mp4?version=4.2.0')
-                 
-@client.command()
-@commands.has_permissions(administrator=True)
-async def byebye(ctx, amount=5):
-  await ctx.channel.purge(limit=amount)
-
-@client.command()
-@commands.has_permissions(administrator=True)
-async def kick(ctx, member : discord.Member, *, reason=None):
-  await member.kick(reason=reason)
-
-@client.command()
+                                                                                                                                                                                                                                           
+@client.command()                                                                                                                                                                                                                          
+async def github(ctx):                                                                                                                                                                                                                     
+  await ctx.send('https://github.com/RealJammy/The-Jambot/blob/master/README.md')                                                                                                                                                          
+                                                                                                                                                                                                                                           
+@client.command()                                                                                                                                                                                                                          
+async def noleek(ctx):                                                                                                                                                                                                                     
+  await ctx.send('https://game.joincyberdiscovery.com/assets/videos/cheating_message.mp4?version=4.2.0')                                                                                                                                   
+                                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                           
+@client.command()                                                                                                                                                                                                                          
+@commands.has_permissions(administrator=True)                                                                                                                                                                                              
+async def byebye(ctx, amount=5):                                                                                                                                                                                                           
+  await ctx.channel.purge(limit=amount)                                                                                                                                                                                                    
+                                                                                                                                                                                                                                           
+@client.command()                                                                                                                                                                                                                          
+@commands.has_permissions(administrator=True)                                                                                                                                                                                              
+async def kick(ctx, member : discord.Member, *, reason=None):                                                                                                                                                                              
+  await member.kick(reason=reason)                                                                                                                                                                                                         
+                                                                                                                                                                                                                                           
+@client.command()                                                                                                                                                                                                                          
 @commands.has_permissions(administrator=True)
 async def ban(ctx, member : discord.Member, *, reason=None):
   await member.ban(reason=reason) 
 
 
-client.run('[put your bot token in here lads]')
+client.run('[put your bot token in ere]')
+
