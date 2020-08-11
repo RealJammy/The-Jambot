4f5c406c4ae7ba8d90402223165473ec95aa4871
willwam845 <45636840+willwam845@users.noreply.github.com>
2020-08-11 23:28:56 +0100

    Update bot.py
    added a space

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_member_join(member):
 
 @client.event
 async def on_member_remove(member):
-  print(f'{member} has left the server.Goodbye!')
+  print(f'{member} has left the server. Goodbye!')
 
 
 @client.command()
