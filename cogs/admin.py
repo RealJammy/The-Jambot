a14337e4be0eb79d6fb318a587086c13d2c07043
themonkeyhacker <jamiethecheekymonkey2@gmail.com>
2020-08-13 08:47:26 +0100

    2/4 admin commands done. added smthn funny to the kick one, i think you'll like it jammy- also i havent slept yet so theres a good chance of spelling errors in here :sunglas:

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,6 +6,8 @@ class admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    #EVENTS
+
     @commands.Cog.listener() #check if this works instead of going off of ur old code jamie
     async def on_ready(self):
         print('The Jambot is here. Hello.')
@@ -26,6 +28,20 @@ class admin(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Please give all the arguments.')
         print(f"ERROR: {error}")
+
+    #ACTUAL ADMIN STUFF (im not screaming, my eyesight is just poor so it helps me see)
+
+    @commmands.command(brief='Clear messages in channel (default 5).', description='Do ".byebye amount_to_clear".')
+    @commands.has_permissions(administrator=True)
+    async def byebye(self, ctx, amount=6): #6 because it needs to clear the command message too
+    await ctx.channel.purge(limit=amount+1)
+
+    @commands.command(brief='Kicks users.', description='Do ".kick @user".')
+    @commands.has_permissions(administrator=True)
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+    user = await commands.get_user_info(member.ID) #not 100% sure this works
+    await member.kick(reason=reason)
+    await commands.send_message(user, "Damn, I'm just a lowly bot but even I think you should have been kicked :/")
 
 def setup(client):
     client.add_cog(admin(client))
