8154f38a3959bb47dbd603864d6b7b194372c606
Jamie Davies <31807451+themonkeyhacker@users.noreply.github.com>
2020-08-15 23:21:53 +0100

    Small change
    Just changed some stuff- this seems better idk- also there is the bug of  it saying "extension reloaded" even if the cog does not exist lol. i dont think this throws an error but havent tested- if someone wants to throw it in a try catch to get easy com dev- go for it. once again havent tested- idk if this throw an error. but it isnt rly a problem. will make only jammy able to use reload soon.

--- bot.py
+++ bot.py
@@ -7,15 +7,11 @@ client = commands.Bot(command_prefix = '.')
 
 @client.command(brief = 'Reloads cog.', description = 'Do ";reload cog".')
 @has_permissions(administrator = True)
-async def reload(ctx, extension):
+async def reload(ctx, extension):    
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
-
     await ctx.send(f'"{extension}" reloaded')
 
-for file in os.listdir('./cogs'):
-    if file.endswith('.py'):
-        client.load_extension(f'cogs.{file[:-3]}')
-#just doing client.load_extension for each cog also works- i just like this :p
-
+client.load_extension('cogs.fun')
+client.load_extension('cogs.admin')
 client.run('')#api key here :)
