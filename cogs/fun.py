38026f2cdbbb18ec38678bb80a7808b26fb89c65
RealJammy <57639251+RealJammy@users.noreply.github.com>
2020-08-15 01:05:55 +0100

    Ben as much as I love you, you need to sleep sometimes.

--- cogs/fun.py
+++ cogs/fun.py
@@ -62,6 +62,12 @@ class fun(commands.Cog):
     @commands.command(brief='to nag rag')
     async def nagrag(self, ctx):
         await ctx.send('Hey <@624713824087572480> this is a nag')
+        
+    @commands.command(brief='To force JSnerd to get some sleep')
+    async def jsnerd(self, ctx):
+        await ctx.send('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <@386245767725056015> get some sleep', tts=True)
+        user = 386245767725056015
+        await commands.send_message(user, "fucking sleep ben ffs")
 
 def setup(client):
     client.add_cog(fun(client))
