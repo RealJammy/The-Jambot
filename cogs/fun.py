b30334a9ec80f5f001749cd4f0554cef276722c8
Jamie Davies <31807451+themonkeyhacker@users.noreply.github.com>
2020-08-15 18:36:53 +0100

    make lb gooder( i think) might have broke this idk- remember to get jammy to test later

--- cogs/fun.py
+++ cogs/fun.py
@@ -89,13 +89,11 @@ class fun(commands.Cog):
         await ctx.send(message)
         
     @commands.command(brief='Decode a number to bytes')
-    async def lb(self, ctx, *args):
+    async def lb(self, ctx, arg):
         try:
-            if len(args) > 0:
-                await ctx.send(long_to_bytes(int(args[0])).decode().replace("@",""))
+            await ctx.send(long_to_bytes(int(arg)).decode().replace("@",""))
         except:
             await ctx.send("Failed <:sex:736200562890113055>")
-
 
 def setup(client):
     client.add_cog(fun(client))
