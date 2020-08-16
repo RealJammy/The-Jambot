baaecf531548e7cf92a56a1cf3e6e54ab6fef325
Jamie Davies <31807451+themonkeyhacker@users.noreply.github.com>
2020-08-16 21:54:17 +0100

    smol bug fixed

--- bot.py
+++ bot.py
@@ -11,9 +11,11 @@ async def reload(ctx, extension):
     if ctx.message.author.id == 448519423901433876:
         client.unload_extension(f'cogs.{extension}')
         client.load_extension(f'cogs.{extension}')
-        await ctx.send(f'Successfully reloaded "{extension}".')
     else:
         await ctx.send("You must be Jammy.")
+        return
+    
+    await ctx.send(f'Successfully reloaded "{extension}".')
 
 client.load_extension('cogs.fun')
 client.load_extension('cogs.admin')
