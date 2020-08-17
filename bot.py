d22c5a7e286c7d19899f7a5f4464d913dc8222ce
themonkeyhacker <jamiethecheekymonkey2@gmail.com>
2020-08-17 15:22:19 +0100

    fixed some stuff (or maybe i broke everything)

--- bot.py
+++ bot.py
@@ -1,9 +1,29 @@
 import discord
-import os
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
 client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready(self):
+    print('The Jambot is here. Hello.')
+    await self.client.change_presence(status=discord.Status.online, activity=discord.Game('Watching over skids'))
+
+@client.event
+async def on_member_join(self, member): 
+    channel = discord.utils.get(message.guild.channels, name='general')
+    await channel.send(f'{member} has joined the server. :smile:')
+
+@client.event
+async def on_member_remove(self, member):
+    channel = discord.utils.get(message.guild.channels, name='general')
+    await channel.send(f'{member} has left the server. :pensive:')
+
+@client.event
+async def on_commmand_error(self, ctx, error): 
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please give all the arguments.')
+    print(f"ERROR: {error}")
 
 @client.command(brief = 'Reloads cog.', description = 'Do ";reload cog".')
 @has_permissions(administrator = True)
--- cogs/admin.py
+++ cogs/admin.py
@@ -1,37 +1,11 @@
 import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions, MissingPermissions
-#import schedule, time #to be used for the last function in here (probably, i think)
 
 class admin(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-
-    #EVENTS
-
-    @commands.Cog.listener() #check if this works instead of going off of ur old code jamie
-    async def on_ready(self):
-        print('The Jambot is here. Hello.')
-        await self.client.change_presence(status=discord.Status.online, activity=discord.Game('Watching over skids'))
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member): #also check if this works lol- will run one big test at the end probably
-        channel = discord.utils.get(message.guild.channels, name='general')
-        await channel.send(f'{member} has joined the server. :smile:')
-
-    @commands.Cog.listener()
-    async def on_member_remove(self, member): #should probably stop putting a note to check at every function lmao
-        channel = discord.utils.get(message.guild.channels, name='general')
-        await channel.send(f'{member} has left the server. :pensive:')
-    
-    @commands.Cog.listener()
-    async def on_commmand_error(self, ctx, error): 
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send('Please give all the arguments.')
-        print(f"ERROR: {error}")
-
-    #ACTUAL ADMIN STUFF (im not screaming, my eyesight is just poor so it helps me see)
 
     @commands.command(brief='Clear messages in channel (default 5).', description='Do ".byebye amount_to_clear".')
     @commands.has_permissions(administrator=True)
@@ -54,8 +28,6 @@ class admin(commands.Cog):
             await commands.send_message(user, "Damn, I'm just a lowly bot but even I think you should have been banned :pensive:")
         except discord.Forbidden:
             await ctx.send("Could not ban user. Check my permissions.")
-
-   # USING AUTOMATED VERSION INSTEAD- JAMMY JUST SAY IF YOU WANT THIS TOO
     
     @commands.command()
     @commands.has_permissions(administrator=True)
