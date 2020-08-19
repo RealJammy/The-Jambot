d27738004f03b9d3b8c9cdcbda90b3c0e7d40686
RealJammy <57639251+RealJammy@users.noreply.github.com>
2020-08-19 17:20:12 +0100

    Update useful.py

--- cogs/useful.py
+++ cogs/useful.py
@@ -16,6 +16,11 @@ class useful(commands.Cog):
             await ctx.send(long_to_bytes(int(arg)).decode().replace("@",""))
         except:
             await ctx.send("Failed <:sex:736200562890113055>")
+            
+    
+    @commands.command(brief='Gives a bot discord invite.')
+    async def invite(self, ctx):
+        await ctx.send('https://discord.com/api/oauth2/authorize?client_id=742802810843693089&permissions=8&scope=bot')
 
 def setup(client):
     client.add_cog(useful(client))
