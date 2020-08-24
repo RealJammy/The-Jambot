62aeb98783f404034ca0752f11ab6bb4b4426a10
RealJammy <57639251+RealJammy@users.noreply.github.com>
2020-08-24 13:02:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,4 +40,5 @@ async def reload(ctx, extension):
 client.load_extension('cogs.fun')
 client.load_extension('cogs.admin')
 client.load_extension('cogs.useful')
+client.load_extension('cogs.requests')
 client.run('')#api key here :)
