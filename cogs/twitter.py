129338f22bed856e1cf2a2f0cc107361d66ab779
PotatoK123 <56174807+PotatoK123@users.noreply.github.com>
2020-10-10 23:10:21 +0100

    Add files via upload

---
+++ cogs/twitter.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands, tasks
+import tweepy
+from datetime import datetime, timedelta
+
+#insert with own values (OAuth 2)
+auth = tweepy.AppAuthHandler(key,secret)
+api = tweepy.API(auth)
+
+accounts = ["jameslyne","CyberDiscUK"]
+
+class Twitter(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def tweet(self, ctx):
+        await ctx.send("Loading tweets in past day... (may not return results)")
+        for account in accounts:
+            for tweet in tweepy.Cursor(api.user_timeline, id=account).items():
+                if tweet.created_at > (datetime.utcnow()-timedelta(hours=24)):
+                    msg = f"@{account} (re)tweeted at {tweet.created_at}\nhttps://twitter.com/twitter/status/{tweet.id}"
+                    await ctx.send(msg)
+                else:
+                    break
+
+def setup(client):
+    client.add_cog(Twitter(client))
