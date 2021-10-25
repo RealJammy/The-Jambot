ad7c8328abf9789817fa9b45de5c2cea688cbf11
woaisthisgit <62507306+woaisthisgit@users.noreply.github.com>
2021-10-25 18:47:55 +0100

    Update useful.py
    Add the exam thing uwu

--- cogs/useful.py
+++ cogs/useful.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands, tasks
 from Crypto.Util.number import *
+from datetime import datetime
+from datetime import timedelta
+import time
 
 
 class Useful(commands.Cog):
@@ -32,10 +35,47 @@ class Useful(commands.Cog):
         await ctx.send("https://github.com/RealJammy/The-Jambot")
         await ctx.send("pls star owo")
 
+
+    @commands.command(brief="Gives a countdown to the chosen exam.")
+    async def exam(self, ctx, arg):
+        
+        examtimes = {
+            "biology": [1636362000],
+            "chemistry": [1635930000],
+            "physics": [1636102800],
+            "music": [1636464600],
+            "economics": [1635859800],
+            "business": [1636378200],
+            "dt": [1636032600],
+            "sports": [1635859800],
+            "maths": [1635757200, 1636448400],
+            "compsci": [1635773400, 1636119000],
+            "english": [1635843600, 1636016400],
+            "geography": [1635765300, 1636110900],
+            "history": [1635946200, 1636456500],
+            "spanish": [1635851700, 1636024500],
+            "art": [1636967700, 1637054100],
+            "rs": [1635938100, 1636370100]
+            }
+        clean = examtimes.get(arg.lower())
+        if clean == "None":
+            await ctx.send("Subject not found!")
+            print(f"The user tried use {arg.lower()}. This obviously doesnt work as it isn't a real subject, right?")
+        else:
+            message = ""
+            clean2 = list(map(time_difference, clean))
+            for i,j in enumerate(clean2):
+                if j:
+                    message += f"The {make_ordinal(i+1)} exam for {arg} will be on {j[0].strftime('%d/%m/%Y at %H:%M')}.\nThat's in {j[1]} days, {j[2]} hours and {j[3]} minutes.\n"
+            if not message:
+                message += "There are no exams left for this subject.\n"
+            await ctx.send(message[:-1])
+           
     @commands.command(brief="Displays this message!")
     async def help(self, ctx):
         cogs = self.client.cogs
-        helpMessage = discord.Embed(title="Help", colour=discord.Colour.purple())
+        helpMessage = discord.Embed(
+            title="Help", colour=discord.Colour.purple())
         for cog in cogs:
             commands = self.client.get_cog(cog).get_commands()
             text = "\n".join(
@@ -45,6 +85,24 @@ class Useful(commands.Cog):
 
         await ctx.send(embed=helpMessage)
 
+def make_ordinal(n):
+    suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
+    if 11 <= (n % 100) <= 13:
+        suffix = 'th'
+    return str(n) + suffix
 
+def time_difference(then):
+    then = datetime.fromtimestamp(then)
+    now = datetime.utcnow()
+    diff = (then - now).total_seconds()
+    if diff <= 0:
+        return None
+    else:
+        minutes = int(diff+59) // 60
+        hours  = minutes // 60
+        days = hours // 24
+        offset = timedelta(hours = 1 if time.localtime().tm_isdst else 0)
+        return [then+offset, days, hours%24, minutes%60]
+    
 def setup(client):
     client.add_cog(Useful(client))
